694d8a99f371a0814cbfc3d725e1b46dd8c104bd
larissamariadesousa <77803@student.dorset-college.ie>
2025-09-30 11:03:53 +0100

    Add files via upload

---
+++ oop-2025-b-eight-ball-77803/oop-2025-b-eight-ball/MagicEightBall.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2025_b_eight_ball
+{
+    internal class MagicEightBall
+    {
+        public static string GetEightBallReponse()
+        {
+            List<string> responses = new List<string>()
+        {
+            "It is certain.",
+            "Without a doubt.",
+            "You may rely on it.",
+            "Yes – definitely.",
+            "Most likely.",
+            "Ask again later.",
+            "Cannot predict now.",
+            "Don't count on it.",
+            "My sources say no.",
+            "Very doubtful."
+        };
+
+            // Create a Random object
+            Random random = new Random();
+
+            // Select a random response
+            int index = random.Next(responses.Count);
+            string answer = responses[index];
+
+            return answer;
+
+        }
+    }
+}
---
+++ oop-2025-b-eight-ball-77803/oop-2025-b-eight-ball/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+// Ask the user to enter a question
+using oop_2025_b_eight_ball;
+
+Console.WriteLine("Ask the Magic Eight Ball a question:");
+string question = Console.ReadLine();
+
+string answer = MagicEightBall.GetEightBallReponse();   
+
+// Show the answer
+Console.WriteLine("\nMagic Eight Ball says: " + answer);
